e38b00a199bdfd7394026ada8250fb493ada4a41
nhorta-g <nunogra@gmail.com>
2024-03-25 16:18:30 +0000

    ex00 complete

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: nuno <nuno@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 16:02:20 by nuno              #+#    #+#             */
-/*   Updated: 2024/03/25 00:35:30 by nuno             ###   ########.fr       */
+/*   Updated: 2024/03/25 16:16:30 by nuno             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,19 @@
 # include <iostream>
 # include <vector>
 
+class NotFoundException: public std::exception {
+	public:
+		const char* what() const throw() {
+			return ("Needle not found.");
+		}
+};
+
 template <typename T>
 typename T::iterator easyfind(T &container, int value) {
 	typename T::iterator it = std::find(container.begin(), container.end(), value);
 	if (it != container.end())
 		return it;
-	throw std::exception();
+	throw NotFoundException();
 }
 
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nuno <nuno@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 16:02:29 by nuno              #+#    #+#             */
-/*   Updated: 2024/03/25 00:48:41 by nuno             ###   ########.fr       */
+/*   Updated: 2024/03/25 16:15:17 by nuno             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,16 @@
 
 int main()
 {
-	int arr[] = {1, 2, 3, 4, 5};
+
+	int arr[] = {1, 2, 3, 4, 8};
 	std::vector<int> vec(arr, arr + sizeof(arr) / sizeof(arr[0]));
+	int num;
 
 	try {
-		std::vector<int>::iterator result = easyfind(vec, 3);
+		std::cout << "To test easyfind function we have an array of ints with 5 elements." << std::endl;
+		std::cout << "Type a number from 0 to 9 to check if its contained and what position" << std::endl;
+		std::cin >> num;
+		std::vector<int>::iterator result = easyfind(vec, num);
 		std::cout << "Value found at index: " << std::distance(vec.begin(), result) << std::endl;
 
 	} catch (const std::exception &e) {
